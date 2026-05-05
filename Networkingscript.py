036080497571afcbd740e12a2aeb88c0d00b4da3
Osasere Asemota <osasereasemota99@gmail.com>
2026-05-04 21:31:12 -0400

    Add network information collection script
    The script collects and saves network information including hostname, IP address, routing, ping test results, tracepath, and netplan configuration files to a text file named with the current date.

--- Networkingscript.py
+++ Networkingscript.py
@@ -3,49 +3,61 @@
 import os
 import datetime
 
-
+# Get current date in YYYY-MM-DD 
 date = datetime.datetime.now().strftime("%Y-%m-%d")
+# Creates a file name using the date and my name
 file = date + "Osasere_network.txt"
 
+# Header info screen
 print("Network Info")
 print("Date:", date)
 print("Test: google.com")
 
+# Open file in write mode creates file
 f = open(file, "w")
 f.write("Network Info\n")
 f.write("Date: " + date + "\n")
 f.write("Testing: google.com\n")
 
+# Show and save hostname (system name)
 print("\nHostname:")
 os.system("hostname")
 f.write("\nHostname:\n")
 os.system("hostname >> " + file)
 
+# Show and save IP address of system
 print("\nIP Address:")
 os.system("hostname -I")
 f.write("\nIP Address:\n")
 os.system("hostname -I >> " + file)
 
+# show and save routing 
 print("\nRouting:")
 os.system("ip route")
 f.write("\nRouting:\n")
 os.system("ip route >> " + file)
 
+# Ping test to check internet connection
 print("\nPing Test:")
 os.system("ping -c 4 google.com")
 f.write("\nPing Test:\n")
 os.system("ping -c 4 google.com >> " + file)
 
+
+# Trace path to show route packets
 print("\nTracepath:")
 os.system("tracepath google.com")
 f.write("\nTracepath:\n")
 os.system("tracepath google.com >> " + file)
 
+# Show and save netplan configuration files
 print("\nNetplan Files:\n")
 os.system("ls /etc/netplan/")
 f.write("\nNetplan Files:\n")
 os.system("ls /etc/netplan/ >> " + file)
 
+# Close file after writing
 f.close()
 
+# Let user know where file was saved
 print("\nSaved to:", file)
